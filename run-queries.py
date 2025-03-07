2f9278aad1101fce0be90d467b4b34cf8a41c73d
Will Anderson <ander39@stolaf.edu>
2025-03-07 03:48:43 -0600

    run queries is not working correctly. There is a problem with the run.subprocess arguments.

--- run-queries.py
+++ run-queries.py
@@ -6,6 +6,8 @@ SOLVER_PATH = "C:/Users/willj/git/cvc5-Win64-x86_64-static/bin"
 
 for query_file in os.listdir(QUERY_FOLDER):     #loops through all query files in the "queries" folder
     #print(query_file)
-    result = subprocess.run(["executable",query_file], capture_output=True, text=True, check=True)
-    print(result)
+    result = subprocess.run(["executable",query_file], capture_output=True, text=True, check=True)  #I am having a problem with the arguments in this, but I can not figure it out
+    #print(result)
+
+    #need code to write the result to a list or directly to a csv
 
